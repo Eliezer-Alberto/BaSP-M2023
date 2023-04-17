99609837cf505924ff00cf0e6f72bcd539b655c6
eliezer-alberto <eliezer.alberto95@gmail.com>
2023-04-17 04:46:51 -0300

    BaSP-W05-2023: strings

--- Week-05/exercises/01-variables-and-operators.js
+++ Week-05/exercises/01-variables-and-operators.js
@@ -1,27 +1,33 @@
+
 /* 1. Variables y Operaciones */
 
-/* a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma
+    console.log('Exercise 1 - Variables y Operadores')
+
+    /* a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma
     de ambos números en una 3er variable.*/
     
+    console.log('1.a')
     var firstNumber = 70;
     var secondNumber = 30;
     var result = (firstNumber + secondNumber); 
     console.log(' The result of this sum is: ' + result);
 
-/* b. Crear dos variables tipo String y concatenarlas guardando el resultado en una 3er variable. */
+    /* b. Crear dos variables tipo String y concatenarlas guardando el resultado en una 3er variable. */
     
+    console.log('1.b')
     String = (nameOne, nameTwo);
     var nameOne = 'Eliezer ';
     var nameTwo = 'Alberto ';
     var namesResult = (nameOne + nameTwo);
     console.log('The result of this operation is: ' + namesResult);
 
-/* c. Crear dos variables tipo String y sumar el largo de cada variable (cantidad de 
+    /* c. Crear dos variables tipo String y sumar el largo de cada variable (cantidad de 
     letras del string) guardando el resultado de la suma en una 3er variable (utilizar lenght). */
 
+    console.log('1.c')
     String = (nameA + nameB);
     var nameA = 'Joaquin';
-    var nameB = 'Luciano'; 
+    var nameB = 'Lucho'; 
     console.log('The length of the variable nameA is: ' + nameA.length);
     console.log('The length of the variable nameB is: ' + nameB.length);
         
